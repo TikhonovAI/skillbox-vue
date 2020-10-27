77b3b5898d88a38684c9182508cfa4b9dd3b6a03
Alexey <Alex29021988@mail.ru>
2020-10-27 20:30:36 +0300

    module 3 done

---
+++ src/data/products.js
@@ -0,0 +1,62 @@
+export default [
+  {
+    title: 'Смартфон APPLE iPhone 11 64Gb, MWLT2RU/A',
+    price: 54990,
+    image: 'img/iphone.jpg',
+  },
+  {
+    title: 'МФУ лазерный PANTUM M6500W',
+    price: 10990,
+    image: 'img/mfu1.jpg',
+  },
+  {
+    title: 'Смартфон SAMSUNG Galaxy A40 64Gb, SM-A405F',
+    price: 12990,
+    image: 'img/galaxy.jpg',
+  },
+  {
+    title: 'Ноутбук ACER Aspire 7 A715-75G-56ZT',
+    price: 62390,
+    image: 'img/acer.jpg',
+  },
+  {
+    title: 'Ноутбук APPLE MacBook Pro 16"',
+    price: 234790,
+    image: 'img/apple.jpg',
+  },
+  {
+    title: 'МФУ лазерный PANTUM M6500W, A4, лазерный, черный',
+    price: 10990,
+    image: 'img/mfu2.jpg',
+  },
+  {
+    title: 'Телевизор XIAOMI Mi TV 4A 32, 31.5", HD READY',
+    price: 11990,
+    image: 'img/xiaomy_tv.jpg',
+  },
+  {
+    title: 'QLED телевизор SAMSUNG QE55Q80TAUXRU',
+    price: 90230,
+    image: 'img/samsung_tv.jpg',
+  },
+  {
+    title: 'Снегоуборщик ИНТЕРСКОЛ СМБ-650Э',
+    price: 38990,
+    image: 'img/interscol.jpg',
+  },
+  {
+    title: 'Подметательная машина PATRIOT MS 70',
+    price: 8290,
+    image: 'img/patriot.jpg',
+  },
+  {
+    title: 'Цифровой фотоаппарат CANON PowerShot SX540 HS',
+    price: 21800,
+    image: 'img/canon.jpg',
+  },
+  {
+    title: 'Фотоаппарат SONY Alpha A7 II body,',
+    price: 110120,
+    image: 'img/sony.jpg',
+  },
+];
